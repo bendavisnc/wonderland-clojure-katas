416c28c1ed969429eaec9f399baa1dc732fc5ca0
ben <bendavis@gmail.com>
2025-03-13 14:32:44 -0400

    Adds back solution.

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -1,8 +1,53 @@
-(ns magic-square.puzzle)
+(ns magic-square.puzzle
+  (:require [clojure.math.combinatorics :as combo]))
 
-(def values [1.0 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0])
+(defn rows-sums [m]
+  (map #(reduce + %) m))
 
-(defn magic-square [values]
-  [[1.0 1.5 2.0]
-   [2.5 3.0 3.5]
-   [4.0 4.5 5.0]])
+(defn cols-sums [m]
+  (map
+    (fn [i]
+      (reduce + (map #(nth % i) m)))
+    (range (count m))))
+
+
+(defn diagonals-sums [m]
+  (let [
+        lr ; diagnol positions, top left -> bottom right
+        (mapv #(vector % %) (range (count m)))
+        rl ; diagnol positions, top right -> bottom left
+        (loop [acc [[0, (dec (count m))]]]
+          (if (= (count lr) (count acc))
+            acc
+            ;else
+            (let [[x, y] (last acc)]
+              (recur (conj acc [(inc x) (dec y)])))))]
+    (mapv
+      (fn [ps]
+        (reduce + (map #(get-in m %) ps)))
+      [lr, rl])))
+
+
+(defn is-magical? [s]
+  (->
+    ((juxt rows-sums, cols-sums, diagonals-sums) s)
+    flatten
+    set
+    count
+    (= 1)))
+
+
+(defn list->sq [l]
+  (->>
+    l
+    (partition 3)
+    (mapv vec)))
+
+(defn magic-square [values] ; brute force attempt
+  (let [all-possible-vals (combo/permutations values)]
+    (loop [values-set all-possible-vals]
+      (let [sq (list->sq (first values-set))]
+        (if (is-magical? sq)
+          sq
+          ;else
+          (recur (rest values-set)))))))
